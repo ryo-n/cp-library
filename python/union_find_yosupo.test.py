cdc1526a705c7cd6c679cb1d183c02b4af1febd1
ryo-n <ryo.nakamura0401@gmail.com>
2020-09-17 15:55:32 +0900

    Add union_find

---
+++ python/union_find_yosupo.test.py
@@ -0,0 +1,17 @@
+# verify-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
+
+from library.union_find import UnionFind
+
+def main():
+    N, Q = [int(x) for x in input().split()]
+    uf = UnionFind(N)
+    for _ in range(Q):
+        t, u, v = [int(x) for x in input().split()]
+        if t == 0:
+            uf.union(u, v)
+        else:
+            print(int(uf.same(u, v)))
+
+    
+if __name__ == "__main__":
+    main()
